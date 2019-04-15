ee39674670364c906a480be81fd3583881f5c37d
dsmastrodomenico <darwin.mastrodomenico@gmail.com>
2019-04-15 17:36:35 -0500

    Creación de Button Sign In Google, Llamado de Metodo GoogleSignOption & ApiCLient, Validacion con onActivityResult y OnCLickListener para Button SignInGoogle

--- app/src/main/java/com/dsmastrodomenico/contactlistapp/LoginAccess.java
+++ app/src/main/java/com/dsmastrodomenico/contactlistapp/LoginAccess.java
@@ -2,6 +2,7 @@ package com.dsmastrodomenico.contactlistapp;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,20 +11,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginAccess extends AppCompatActivity {
+public class LoginAccess extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = "LoginAccess";
+    private static final int SIGN_IN_GOOGLE_CODE = 1;
+
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private GoogleApiClient googleApiClient;
+
     private Button btnCreateAccount;
     private Button btnSignIn;
     private SignInButton btnSignInGoogle;
+
     private EditText edtEmail;
     private EditText edtPassword;
 
@@ -34,9 +48,12 @@ public class LoginAccess extends AppCompatActivity {
         btnCreateAccount = (Button)findViewById(R.id.btnCreateAccount);
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
         btnSignInGoogle = (SignInButton)findViewById(R.id.btnSignInGoogle);
+
         edtEmail =(EditText)findViewById(R.id.edtEmail);
         edtPassword =(EditText)findViewById(R.id.edtPassword);
-        inicialize();
+
+        initialize();
+
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,9 +66,17 @@ public class LoginAccess extends AppCompatActivity {
                 signIn(edtEmail.getText().toString(), edtPassword.getText().toString());
             }
         });
+        btnSignInGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN_GOOGLE_CODE);
+            }
+        });
     }
 
-    private void inicialize() {
+    private void initialize() {
+        //Validación de Usuario mediante Firebase Authentification
         firebaseAuth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -65,6 +90,17 @@ public class LoginAccess extends AppCompatActivity {
                 }
             }
         };
+        //Inicialización de Google Account.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("456917595223-dukhi1lf8hd7vhp8i5v2i9il2bgcnu3u.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
 
     private void createAccount(String email, String password){
@@ -105,6 +141,29 @@ public class LoginAccess extends AppCompatActivity {
         });
     }
 
+    private void signInGoogleFirebase(GoogleSignInResult googleSignInResult){
+        if (googleSignInResult.isSuccess()){
+            AuthCredential authCredential =
+                    GoogleAuthProvider.getCredential(googleSignInResult.getSignInAccount().getIdToken(), null);
+            firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(LoginAccess.this, R.string.authentication_success, Toast.LENGTH_SHORT).show();;
+                        Intent access = new Intent(LoginAccess.this, MainMenu.class);
+                        startActivity(access);
+                        finish();
+
+                    }else{
+                        Toast.makeText(LoginAccess.this, R.string.authentication_unsuccess, Toast.LENGTH_SHORT).show();;
+                    }
+                }
+            });
+        }else{
+
+        }
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -115,5 +174,20 @@ public class LoginAccess extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN_GOOGLE_CODE){
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            signInGoogleFirebase(googleSignInResult);
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
